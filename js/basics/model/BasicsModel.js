1135c866b2aa33aabde20b6d71f21fc12f743cf6
Chris Malley <cmalley@pixelzoom.com>
2013-11-25 17:13:53 -0500

    Basics: move water faucet down a bit

--- js/basics/model/BasicsModel.js
+++ js/basics/model/BasicsModel.js
@@ -45,7 +45,7 @@ define( function( require ) {
     var yDropper = 260;
     thisModel.dropper = new Dropper( thisModel.solutes[0], new Vector2( thisModel.beaker.location.x + 40, yDropper ), new Bounds2( 250, yDropper, 510, yDropper ) );
     thisModel.solution = new Solution( thisModel.dropper.soluteProperty, 0, thisModel.solvent, 0, thisModel.beaker.volume );
-    thisModel.solventFaucet = new Faucet( new Vector2( 185, 190 ), -400,
+    thisModel.solventFaucet = new Faucet( new Vector2( 185, 230 ), -400,
       { enabled: thisModel.solution.volumeProperty.get() < thisModel.beaker.volume } );
     thisModel.drainFaucet = new Faucet( new Vector2( thisModel.beaker.right + 100, 652 ), thisModel.beaker.right,
       { enabled: thisModel.solution.volumeProperty.get() > 0 } );

37ca8107072b6b8d0af6df12dfb84a3e5177f9b4
Chris Malley <cmalley@pixelzoom.com>
2013-11-26 10:45:50 -0500

    add color-coding to 'H3O+/OH- ratio' label

--- js/common/view/BeakerControls.js
+++ js/common/view/BeakerControls.js
@@ -13,14 +13,16 @@ define( function( require ) {
   var HTMLText = require( 'SCENERY/nodes/HTMLText' );
   var inherit = require( 'PHET_CORE/inherit' );
   var Line = require( 'SCENERY/nodes/Line' );
+  var Node = require( 'SCENERY/nodes/Node' );
   var Panel = require( 'SUN/Panel' );
   var PhetFont = require( 'SCENERY_PHET/PhetFont' );
+  var PHScaleColors = require( 'PH_SCALE/common/PHScaleColors' );
   var Text = require( 'SCENERY/nodes/Text' );
   var VBox = require( 'SCENERY/nodes/VBox' );
 
   // strings
-  var moleculeCountString = 'Molecule count'; //TODO i18n
-  var ratioString = 'H<sub>3</sub>O<sup>+</sup>/OH<sup>-</sup> ratio';
+  var moleculeCountString = require( 'string!PH_SCALE/moleculeCount' );
+  var ratioString = require( 'string!PH_SCALE/ratio' );
 
   // constants
   var FONT = new PhetFont( 20 );
@@ -32,7 +34,24 @@ define( function( require ) {
    */
   function BeakerControls( ratioVisibleProperty, moleculeCountVisibleProperty ) {
 
-    var ratioLabel = new HTMLText( ratioString, { font: FONT } );
+    //TODO the ordering of this label is not localized
+    // Label with color-coded formulas
+    var textH3O = new HTMLText( 'H<sub>3</sub>O<sup>+</sup>', { font: FONT, fill: PHScaleColors.ACIDIC } );
+    var textSlash = new HTMLText( '/', { font: FONT } );
+    var textOH = new HTMLText( 'OH<sup>-</sup>', { font: FONT, fill: PHScaleColors.BASIC } );
+    var textRatio = new HTMLText( ratioString, { font: FONT } );
+    var ratioLabel = new Node();
+    ratioLabel.addChild( textH3O );
+    ratioLabel.addChild( textSlash );
+    ratioLabel.addChild( textOH );
+    ratioLabel.addChild( textRatio );
+    textSlash.left = textH3O.right + 1;
+    textOH.left = textSlash.right + 1;
+    textRatio.left = textOH.right + 6;
+    //TODO how to align baselines of Text and HTMLText?
+    textH3O.y = textH3O.y - 4;
+    textOH.y = textOH.y - 4;
+
     var ratioCheckBox = new CheckBox( ratioLabel, ratioVisibleProperty );
 
     var moleculeCountLabel = new Text( moleculeCountString, { font: FONT } );

90ba1878ae2f1821f1cbd5aadadb8a0766d1408f
Chris Malley <cmalley@pixelzoom.com>
2013-12-18 11:21:32 -0700

    more doc

--- js/common/view/BeakerControls.js
+++ js/common/view/BeakerControls.js
@@ -1,7 +1,8 @@
 // Copyright 2002-2013, University of Colorado Boulder
 
 /**
- * Controls for the beaker.
+ * Controls the affect what you see in the beaker.
+ * This includes the ''H3O+/OH- ratio' and 'Molecule count' representations.
  *
  * @author Chris Malley (PixelZoom, Inc.)
  */

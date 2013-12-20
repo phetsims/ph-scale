b950df90ae806c4f7b191b1b7e8589cf4cec8599
Chris Malley <cmalley@pixelzoom.com>
2013-12-20 16:24:50 -0700

    cleanup of color-stop in Solute

--- js/common/model/Solute.js
+++ js/common/model/Solute.js
@@ -30,13 +30,13 @@ define( function( require ) {
   /**
    * @param {String} name
    * @param {Number} pH
-   * @param {*} colorScheme
+   * @param {stockColor:Color, [dilutedColor]:Color, colorStop:{color:{Color}, [ratio]:Number} } colorScheme
    *
    * colorScheme is an object literal with these properties:
-   * {Color} stockColor: color of the solute in stock solution (no dilution)
-   * {Color} dilutedColor: color when the solute is barely present in solution (fully diluted), optional, defaults to Water.color
-   * {Color} colorStopColor: color when colorStopRatio === soluteVolume/totalVolume, used to smooth out some color transitions if provided, optional
-   * {Number} colorStopRatio: (0,1) optional
+   * stockColor: color of the solute in stock solution (no dilution)
+   * dilutedColor: color when the solute is barely present in solution (fully diluted), optional, defaults to Water.color
+   * color: color when soluteVolume/totalVolume === ratio, used to smooth out some color transitions if provided, optional
+   * ratio: ratio for the color-stop, (0,1) exclusive, optional, defaults to 0.25
    */
   function Solute( name, pH, colorScheme ) {
 
@@ -46,25 +46,28 @@ define( function( require ) {
     // unpack the colors to make accessing them more convenient in client code
     this.stockColor = colorScheme.stockColor;
     this.dilutedColor = colorScheme.dilutedColor || Water.color;
-    this.colorStopColor = colorScheme.colorStopColor; // optional, color computation will ignore it if undefined
-    this.colorStopRatio = colorScheme.colorStopRatio || 0.25;
+    this.colorStop = colorScheme.colorStop; // optional, color computation will ignore it if undefined
+    if ( this.colorStop ) {
+      this.colorStop.ratio = this.colorStop.ratio || 0.25;
+    }
   }
 
   Solute.prototype = {
     /**
      * Computes the color for a dilution of this solute.
-     * @param {Number} ratio describes the dilution, range is [0,1], 0 is no solute, 1 is all solute
+     * @param {Number} ratio describes the dilution, range is [0,1] inclusive, 0 is no solute, 1 is all solute
      * @returns {Color}
      */
     computeColor: function( ratio ) {
+      assert && assert( ratio >= 0 && ratio <= 1 );
       var color;
-      if ( this.colorStopRatio ) {
+      if ( this.colorStop ) {
         // solute has an optional color-stop
-        if ( ratio > this.colorStopRatio ) {
-          color = Color.interpolateRBGA( this.colorStopColor, this.stockColor, ( ratio - this.colorStopRatio ) / ( 1 - this.colorStopRatio) );
+        if ( ratio > this.colorStop.ratio ) {
+          color = Color.interpolateRBGA( this.colorStop.color, this.stockColor, ( ratio - this.colorStop.ratio ) / ( 1 - this.colorStop.ratio) );
         }
         else {
-          color = Color.interpolateRBGA( this.dilutedColor, this.colorStopColor, ratio / this.colorStopRatio );
+          color = Color.interpolateRBGA( this.dilutedColor, this.colorStop.color, ratio / this.colorStop.ratio );
         }
       }
       else {
@@ -75,17 +78,54 @@ define( function( require ) {
   };
 
   // 'real world' immutable solutions
-  Solute.DRAIN_CLEANER = new Solute( drainCleanerString, 13, { stockColor: new Color( 255, 255, 0 ), colorStopColor: new Color( 255, 255, 204 ) } );
-  Solute.HAND_SOAP = new Solute( handSoapString, 10, { stockColor: new Color( 204, 0, 204 ), colorStopColor: new Color( 232, 204, 255 ) } );
-  Solute.BLOOD = new Solute( bloodString, 7.4, { stockColor: new Color( 185, 12, 0 ), colorStopColor: new Color( 255, 207, 204 ) } );
+  Solute.DRAIN_CLEANER = new Solute( drainCleanerString, 13, {
+    stockColor: new Color( 255, 255, 0 ),
+    colorStop: { color: new Color( 255, 255, 204 ) }
+  } );
+
+  Solute.HAND_SOAP = new Solute( handSoapString, 10, {
+    stockColor: new Color( 204, 0, 204 ),
+    colorStop: { color: new Color( 232, 204, 255 ) }
+  } );
+
+  Solute.BLOOD = new Solute( bloodString, 7.4, {
+    stockColor: new Color( 185, 12, 0 ),
+    colorStop: { color: new Color( 255, 207, 204 ) }
+  } );
+
   Solute.SPIT = new Solute( spitString, 7.4, { stockColor: new Color( 202, 240, 239 ) } );
+
   Solute.MILK = new Solute( milkString, 6.5, { stockColor: new Color( 250, 250, 250 ) } );
-  Solute.CHICKEN_SOUP = new Solute( chickenSoupString, 5.8, { stockColor: new Color( 255, 240, 104 ), colorStopColor: new Color( 255, 250, 204 ) } );
-  Solute.COFFEE = new Solute( coffeeString, 5.0, { stockColor: new Color( 164, 99, 7 ), colorStopColor: new Color( 255, 240, 204 ) } );
-  Solute.BEER = new Solute( beerString, 4.5, { stockColor: new Color( 255, 200, 0 ), colorStopColor: new Color( 255, 242, 204 ) } );
-  Solute.SODA = new Solute( sodaString, 2.5, { stockColor: new Color( 204, 255, 102 ), colorStopColor: new Color( 238, 255, 204 ) } );
-  Solute.VOMIT = new Solute( vomitString, 2, { stockColor: new Color( 255, 171, 120 ), colorStopColor: new Color( 255, 224, 204 ) } );
-  Solute.BATTERY_ACID = new Solute( batteryAcidString, 1, { stockColor: new Color( 255, 255, 0 ), colorStopColor: new Color( 255, 224, 204 ) } );
+
+  Solute.CHICKEN_SOUP = new Solute( chickenSoupString, 5.8, {
+    stockColor: new Color( 255, 240, 104 ),
+    colorStop: { color: new Color( 255, 250, 204 ) }
+  } );
+
+  Solute.COFFEE = new Solute( coffeeString, 5, {
+    stockColor: new Color( 164, 99, 7 ),
+    colorStop: { color: new Color( 255, 240, 204 ) }
+  } );
+
+  Solute.BEER = new Solute( beerString, 4.5, {
+    stockColor: new Color( 255, 200, 0 ),
+    colorStop: { color: new Color( 255, 242, 204 ) }
+  } );
+
+  Solute.SODA = new Solute( sodaString, 2.5, {
+    stockColor: new Color( 204, 255, 102 ),
+    colorStop: { color: new Color( 238, 255, 204 ) }
+  } );
+
+  Solute.VOMIT = new Solute( vomitString, 2, {
+    stockColor: new Color( 255, 171, 120 ),
+    colorStop: { color: new Color( 255, 224, 204 ) }
+  } );
+
+  Solute.BATTERY_ACID = new Solute( batteryAcidString, 1, {
+    stockColor: new Color( 255, 255, 0 ),
+    colorStop: { color: new Color( 255, 224, 204 ) }
+  } );
 
   // 'magical' mutable solution
   Solute.CUSTOM = new Solute( customString, 7, { stockColor: new Color( 251, 236, 150 ) } );

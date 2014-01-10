ec2747592fdf966ebd542eee352211ee50227d21
Chris Malley <cmalley@pixelzoom.com>
2014-01-09 19:49:54 -0700

    move code for displaying pH value into BasicsPHMeterNode, since it no longer shares design with the other screens

--- js/basics/view/BasicsPHMeterNode.js
+++ js/basics/view/BasicsPHMeterNode.js
@@ -20,6 +20,7 @@ define( function( require ) {
   'use strict';
 
   // imports
+  var Color = require( 'SCENERY/util/Color' );
   var Dimension2 = require( 'DOT/Dimension2' );
   var Image = require( 'SCENERY/nodes/Image' );
   var inherit = require( 'PHET_CORE/inherit' );
@@ -27,19 +28,108 @@ define( function( require ) {
   var MovableDragHandler = require( 'SCENERY_PHET/input/MovableDragHandler' );
   var Node = require( 'SCENERY/nodes/Node' );
   var Path = require( 'SCENERY/nodes/Path' );
+  var PhetFont = require( 'SCENERY_PHET/PhetFont' );
+  var PHScaleColors = require( 'PH_SCALE/common/PHScaleColors' );
   var PHScaleConstants = require( 'PH_SCALE/common/PHScaleConstants' );
   var PHScaleNode = require( 'PH_SCALE/common/view/PHScaleNode' );
-  var PHValueNode = require( 'PH_SCALE/common/view/PHValueNode' );
   var Property = require( 'AXON/Property' );
+  var Rectangle = require( 'SCENERY/nodes/Rectangle' );
   var Shape = require( 'KITE/Shape' );
+  var Text = require( 'SCENERY/nodes/Text' );
   var Util = require( 'DOT/Util' );
   var Vector2 = require( 'DOT/Vector2' );
 
   // images
   var probeImage = require( 'image!PH_SCALE/pH-meter-probe.png' );
 
+  // strings
+  var pHString = require( 'string!PH_SCALE/pH' );
+  var stringNoValue = '-';
+
   // constants
   var SCALE_SIZE = new Dimension2( 55, 450 );
+
+  /**
+   * Displays pH value inside of a rounded rectangle, which is then placed inside of yet-another rounded rectangle.
+   * This is the thing that you see sliding up and down the pH Scale.
+   * @param {Property<Number>} pHProperty
+   * @param {Property<Boolean>} enabledProperty
+   * @constructor
+   */
+  function ValueNode( pHProperty, enabledProperty ) {
+
+    var thisNode = this;
+    Node.call( thisNode );
+
+    // pH value
+    var valueNode = new Text( Util.toFixed( PHScaleConstants.PH_RANGE.max, PHScaleConstants.PH_METER_DECIMAL_PLACES ),
+      { fill: 'black', font: new PhetFont( 28 ) } );
+
+    // rectangle that the value is displayed in
+    var valueXMargin = 8;
+    var valueYMargin = 5;
+    var cornerRadius = 12;
+    var valueRectangle = new Rectangle( 0, 0, valueNode.width + ( 2 * valueXMargin ), valueNode.height + ( 2 * valueYMargin ), cornerRadius, cornerRadius,
+      { fill: 'white' } );
+
+    // label above the value
+    var labelNode = new Text( pHString,
+      { fill: 'white', font: new PhetFont( { size: 28, weight: 'bold' } ) } );
+
+    // background
+    var backgroundXMargin = 14;
+    var backgroundYMargin = 10;
+    var backgroundYSpacing = 6;
+    var backgroundWidth = Math.max( labelNode.width, valueRectangle.width ) + ( 2 * backgroundXMargin );
+    var backgroundHeight = labelNode.height + valueRectangle.height + backgroundYSpacing + ( 2 * backgroundYMargin );
+    var backgroundRectangle = new Rectangle( 0, 0, backgroundWidth, backgroundHeight, cornerRadius, cornerRadius,
+      { fill: new Color( 31, 113, 2 ) } );
+
+    // highlight around the background
+    var highlightLineWidth = 3;
+    var outerHighlight = new Rectangle( 0, 0, backgroundWidth, backgroundHeight, cornerRadius, cornerRadius,
+      { stroke: 'black', lineWidth: highlightLineWidth } );
+    var innerHighlight = new Rectangle( highlightLineWidth, highlightLineWidth, backgroundWidth - ( 2 * highlightLineWidth ), backgroundHeight - ( 2 * highlightLineWidth ), cornerRadius, cornerRadius,
+      { stroke: 'white', lineWidth: highlightLineWidth } );
+    var highlight = new Node( { children: [ innerHighlight, outerHighlight ], visible: false } );
+
+    // rendering order
+    thisNode.addChild( backgroundRectangle );
+    thisNode.addChild( highlight );
+    thisNode.addChild( valueRectangle );
+    thisNode.addChild( labelNode );
+    thisNode.addChild( valueNode );
+
+    // layout
+    labelNode.centerX = backgroundRectangle.centerX;
+    labelNode.top = backgroundRectangle.top + backgroundYMargin;
+    valueRectangle.centerX = backgroundRectangle.centerX;
+    valueRectangle.top = labelNode.bottom + backgroundYSpacing;
+    valueNode.right = valueRectangle.right - valueXMargin; // right justified
+    valueNode.centerY = valueRectangle.centerY;
+
+    // pH value
+    pHProperty.link( function( pH ) {
+      if ( pH === null ) {
+        valueNode.text = stringNoValue;
+        valueNode.centerX = valueRectangle.centerX; // center justified
+        highlight.visible = false;
+      }
+      else {
+        valueNode.text = Util.toFixed( pH, PHScaleConstants.PH_METER_DECIMAL_PLACES );
+        valueNode.right = valueRectangle.right - valueXMargin; // right justified
+        highlight.visible = ( parseFloat( valueNode.text ) === 7 );
+      }
+    } );
+
+    if ( enabledProperty ) {
+      enabledProperty.link( function( enabled ) {
+        backgroundRectangle.fill = enabled ? PHScaleColors.PH_DISPLAY : 'rgb(178,178,178)';
+      } );
+    }
+  }
+
+  inherit( Node, ValueNode );
 
   /**
    * Meter probe, origin at center of crosshairs.
@@ -164,7 +254,7 @@ define( function( require ) {
 
     // value
     var valueEnabled = new Property( true );
-    var valueNode = new PHValueNode( pHProperty, valueEnabled );
+    var valueNode = new ValueNode( pHProperty, valueEnabled );
 
     // arrow head pointing at the scale
     var arrowSize = new Dimension2( 21, 28 );

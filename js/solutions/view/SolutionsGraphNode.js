bf57def42e25eefc8a1696b5b996d6cb24e2fbde
Chris Malley <cmalley@pixelzoom.com>
2014-01-23 23:11:07 -0700

    move linear graph to Solutions screen

--- js/solutions/view/SolutionsGraphNode.js
+++ js/solutions/view/SolutionsGraphNode.js
@@ -1,8 +1,10 @@
 // Copyright 2002-2013, University of Colorado Boulder
 
+//TODO some duplication with CustomGraphNode
 /**
  * The graph for the 'Solutions' screen.
  * It has an expand/collapse bar at the top of it, and can switch between 'concentration' and 'quantity'.
+ * Below the graph is a switch for 'logarithmic' vs 'linear' scales.
  * The graph indicators are not interactive because the stock solutions (solutes) are immutable.
  * Origin is at top-level of the expand/collapse bar.
  *
@@ -15,9 +17,11 @@ define( function( require ) {
   var ABSwitch = require( 'SUN/ABSwitch' );
   var Dimension2 = require( 'DOT/Dimension2' );
   var ExpandCollapseBar = require( 'SUN/ExpandCollapseBar' );
+  var GraphScale = require( 'PH_SCALE/common/view/graph/GraphScale' );
   var GraphUnits = require( 'PH_SCALE/common/view/graph/GraphUnits' );
   var inherit = require( 'PHET_CORE/inherit' );
   var Line = require( 'SCENERY/nodes/Line' );
+  var LinearGraph = require( 'PH_SCALE/common/view/graph/LinearGraph' );
   var LogarithmicGraph = require( 'PH_SCALE/common/view/graph/LogarithmicGraph' );
   var MultiLineText = require( 'SCENERY_PHET/MultiLineText' );
   var Node = require( 'SCENERY/nodes/Node' );
@@ -25,9 +29,13 @@ define( function( require ) {
   var PHScaleColors = require( 'PH_SCALE/common/PHScaleColors' );
   var PHScaleConstants = require( 'PH_SCALE/common/PHScaleConstants' );
   var Property = require( 'AXON/Property' );
+  var Text = require( 'SCENERY/nodes/Text' );
+  var ZoomButton = require( 'PH_SCALE/common/view/graph/ZoomButton' );
 
   // strings
   var concentrationString = require( 'string!PH_SCALE/concentration' );
+  var linearString = require( 'string!PH_SCALE/linear' );
+  var logarithmicString = require( 'string!PH_SCALE/logarithmic' );
   var molesString = require( 'string!PH_SCALE/units.moles' );
   var molesPerLiterString = require( 'string!PH_SCALE/units.molesPerLiter' );
   var quantityString = require( 'string!PH_SCALE/quantity' );
@@ -41,15 +49,17 @@ define( function( require ) {
 
     options = _.extend( {
       expanded: true,
-      units: GraphUnits.MOLES_PER_LITER
+      units: GraphUnits.MOLES_PER_LITER,
+      graphScale: GraphScale.LOGARITHMIC
     }, options );
 
     var thisNode = this;
     Node.call( thisNode );
 
+    var textOptions = { font: new PhetFont( { size: 18, weight: 'bold' } ) };
+
     // units switch
     var graphUnitsProperty = new Property( options.units );
-    var textOptions = { font: new PhetFont( { size: 18, weight: 'bold' } ) };
     var graphUnitsSwitch = new ABSwitch( graphUnitsProperty,
       GraphUnits.MOLES_PER_LITER, new MultiLineText( concentrationString + '\n(' + molesPerLiterString + ')', textOptions ),
       GraphUnits.MOLES, new MultiLineText( quantityString + '\n(' + molesString + ')', textOptions ),
@@ -67,28 +77,76 @@ define( function( require ) {
         buttonLength: PHScaleConstants.EXPAND_COLLAPSE_BUTTON_LENGTH
       } );
 
-    // vertical line that connects graph to expand/collapse bar
-    var lineNode = new Line( 0, 0, 0, 30, { stroke: 'black' } );
-
     // logarithmic graph, switchable between 'concentration' and 'quantity'
+    var scaleHeight = 475;
     var logarithmicGraph = new LogarithmicGraph( solution, graphUnitsProperty, {
-      scaleHeight: 530,
+      scaleHeight: scaleHeight,
       isInteractive: false
     } );
 
+    // linear graph, switchable between 'concentration' and 'quantity'
+    var linearGraph = new LinearGraph( solution, graphUnitsProperty, {
+      scaleHeight: scaleHeight
+    } );
+
+    // zoom buttons for the linear graph
+    var magnifyingGlassRadius = 10;
+    var zoomInButton = new ZoomButton( { in: true, radius: magnifyingGlassRadius } );
+    var zoomOutButton = new ZoomButton( { in: false, radius: magnifyingGlassRadius } );
+    var zoomButtons = new Node( { children: [ zoomInButton, zoomOutButton ]} );
+    zoomOutButton.left = zoomInButton.right + 10;
+    zoomOutButton.centerY = zoomInButton.centerY;
+
+    // switch between 'Logarithmic' and 'Linear'
+    var graphScaleProperty = new Property( options.graphScale );
+    var graphScaleSwitch = new ABSwitch( graphScaleProperty,
+      GraphScale.LOGARITHMIC, new Text( logarithmicString, textOptions ),
+      GraphScale.LINEAR, new Text( linearString, textOptions ),
+      { size: new Dimension2( 50, 25 ), centerOnButton: true } );
+
+    // vertical line that connects graph to expand/collapse bar
+    var lineNode = new Line( 0, 0, 0, 30, { stroke: 'black' } );
+
     // rendering order
     thisNode.addChild( expandCollapseBar );
-    thisNode.addChild( lineNode );
-    thisNode.addChild( logarithmicGraph );
+    var graphNode = new Node();
+    thisNode.addChild( graphNode );
+    graphNode.addChild( lineNode );
+    graphNode.addChild( logarithmicGraph );
+    graphNode.addChild( linearGraph );
+    graphNode.addChild( zoomButtons );
+    graphNode.addChild( graphScaleSwitch );
 
     // layout
-    lineNode.centerX = expandCollapseBar.centerX;
-    lineNode.top = expandCollapseBar.bottom - 1;
     logarithmicGraph.centerX = lineNode.centerX;
     logarithmicGraph.y = lineNode.bottom - 1; // y, not top
+    linearGraph.centerX = logarithmicGraph.centerX;
+    linearGraph.y = logarithmicGraph.y; // y, not top
+    zoomButtons.centerX = logarithmicGraph.centerX;
+    zoomButtons.top = lineNode.bottom + scaleHeight + 15;
+    graphScaleSwitch.centerX = zoomButtons.centerX;
+    graphScaleSwitch.top = zoomButtons.bottom + 15;
+    graphNode.centerX = expandCollapseBar.centerX;
+    graphNode.top = expandCollapseBar.bottom;
 
     expandedProperty.link( function( expanded ) {
-      logarithmicGraph.visible = lineNode.visible = expanded;
+      graphNode.visible = expanded;
+    } );
+
+    // handle scale changes
+    graphScaleProperty.link( function( graphScale ) {
+      logarithmicGraph.visible = ( graphScale === GraphScale.LOGARITHMIC );
+      linearGraph.visible = zoomButtons.visible = ( graphScale === GraphScale.LINEAR );
+    } );
+
+    // handle zoom of linear graph
+    zoomInButton.addListener( function() {
+      //TODO zoom in
+      console.log( 'zoom in' );
+    } );
+    zoomOutButton.addListener( function() {
+      //TODO zoom out
+      console.log( 'zoom out' );
     } );
   }
 

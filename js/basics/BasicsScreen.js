94f82f8c806141496b5d0be3e0db97c650823c37
Chris Malley <cmalley@pixelzoom.com>
2014-01-23 19:12:45 -0700

    add temporary icon for 'Basics' screen

--- js/basics/BasicsScreen.js
+++ js/basics/BasicsScreen.js
@@ -11,19 +11,22 @@ define( function( require ) {
   // imports
   var BasicsModel = require( 'PH_SCALE/basics/model/BasicsModel' );
   var BasicsView = require( 'PH_SCALE/basics/view/BasicsView' );
+  var Image = require( 'SCENERY/nodes/Image' );
   var inherit = require( 'PHET_CORE/inherit' );
   var ModelViewTransform2 = require( 'PHETCOMMON/view/ModelViewTransform2' );
   var PHScaleColors = require( 'PH_SCALE/common/PHScaleColors' );
-  var Rectangle = require( 'SCENERY/nodes/Rectangle' );
   var Screen = require( 'JOIST/Screen' );
 
   // strings
   var screenTitle = require( 'string!PH_SCALE/basics' );
 
+  // images
+  var screenIcon = require( 'image!PH_SCALE/Basics-screen-icon.png' );
+
   function BasicsScreen() {
     Screen.call( this,
       screenTitle,
-      new Rectangle( 0, 0, 548, 373, { fill: 'green', stroke: 'black' } ), //TODO placeholder
+      new Image( screenIcon ),
       function() { return new BasicsModel(); },
       function( model ) { return new BasicsView( model, ModelViewTransform2.createIdentity() ); },
       { backgroundColor: PHScaleColors.SCREEN_BACKGROUND }

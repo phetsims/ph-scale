df77e62d681abd35a38fa5a4141bcec12cc7cb48
Chris Malley <cmalley@pixelzoom.com>
2014-01-23 20:54:47 -0700

    Solutions: remove scale from pH meter

--- js/solutions/view/SolutionsPHMeterNode.js
+++ js/solutions/view/SolutionsPHMeterNode.js
@@ -12,19 +12,14 @@ define( function( require ) {
   'use strict';
 
   // imports
-  var Dimension2 = require( 'DOT/Dimension2' );
   var ExpandCollapseButton = require( 'SUN/ExpandCollapseButton' );
   var inherit = require( 'PHET_CORE/inherit' );
-  var Line = require( 'SCENERY/nodes/Line' );
   var Node = require( 'SCENERY/nodes/Node' );
-  var Path = require( 'SCENERY/nodes/Path' );
   var PhetFont = require( 'SCENERY_PHET/PhetFont' );
   var PHScaleColors = require( 'PH_SCALE/common/PHScaleColors' );
   var PHScaleConstants = require( 'PH_SCALE/common/PHScaleConstants' );
-  var PHScaleNode = require( 'PH_SCALE/common/view/PHScaleNode' );
   var Property = require( 'AXON/Property' );
   var Rectangle = require( 'SCENERY/nodes/Rectangle' );
-  var Shape = require( 'KITE/Shape' );
   var Text = require( 'SCENERY/nodes/Text' );
   var Util = require( 'DOT/Util' );
 
@@ -117,45 +112,6 @@ define( function( require ) {
   inherit( Node, ValueNode );
 
   /**
-   * Arrow and dashed line that points to a value on the pH scale.
-   * @param {Property<Number>} scaleWidth
-   * @constructor
-   */
-  function PointerNode( scaleWidth ) {
-
-    var thisNode = this;
-    Node.call( thisNode );
-
-    // dashed line that extends across the scale
-    var lineNode = new Line( 0, 0, scaleWidth, 0, {
-      stroke: 'black',
-      lineDash: [ 5, 5 ],
-      lineWidth: 2
-    } );
-
-    // arrow head pointing at the scale
-    var arrowSize = new Dimension2( 21, 28 );
-    var arrowNode = new Path( new Shape()
-      .moveTo( 0, 0 )
-      .lineTo( arrowSize.width, -arrowSize.height / 2 )
-      .lineTo( arrowSize.width, arrowSize.height / 2 )
-      .close(),
-      { fill: 'black' } );
-
-    // rendering order
-    thisNode.addChild( arrowNode );
-    thisNode.addChild( lineNode );
-
-    // layout, origin at arrow tip
-    lineNode.left = 0;
-    lineNode.centerY = 0;
-    arrowNode.left = lineNode.right;
-    arrowNode.centerY = lineNode.centerY;
-  }
-
-  inherit( Node, PointerNode );
-
-  /**
    * @param {Property<Number>} pHProperty
    * @param {Number} scaleBottom y-offset of the bottom of the scale, in this node's coordinate frame
    * @param {Number} scaleTop y-offset of the top of the scale, in this node's coordinate frame
@@ -166,8 +122,6 @@ define( function( require ) {
 
     options = _.extend( { expanded: true }, options );
 
-    var SCALE_SIZE = new Dimension2( 55, scaleBottom - scaleTop );
-
     var thisNode = this;
     Node.call( thisNode );
 
@@ -175,31 +129,18 @@ define( function( require ) {
 
     // nodes
     var valueNode = new ValueNode( pHProperty, expandedProperty );
-    var verticalLineNode = new Line( 0, 0, 0, scaleTop - valueNode.height + 2, { stroke: 'black', lineWidth: 5 } );
-    var scaleNode = new PHScaleNode( { size: SCALE_SIZE } );
-    var pointerNode = new PointerNode( SCALE_SIZE.width );
+    //TODO add probe
 
     // rendering order
-    var meterNode = new Node( { children:[ verticalLineNode, valueNode, scaleNode, pointerNode ] } );
+    var meterNode = new Node( { children:[ valueNode ] } );
     thisNode.addChild( meterNode );
+    //TODO thisNode.addChild( probeNode );
 
     // layout
-    scaleNode.right = valueNode.centerX + ( SCALE_SIZE.width / 2 );
-    scaleNode.y = scaleTop; // set scaleNode.y instead of scaleNode.top so that we don't have to compensate for stroke width
-    verticalLineNode.centerX = valueNode.centerX;
-    verticalLineNode.bottom = scaleNode.top + 1;
-    pointerNode.x = scaleNode.right - SCALE_SIZE.width;
-    // pointerNode.centerY is set dynamically
-
-    // move the pointer to the pH value
-    pHProperty.link( function( value ) {
-      pointerNode.visible = ( expandedProperty.get() && value !== null );
-      pointerNode.centerY = scaleNode.top + ( scaleNode.getBackgroundStrokeWidth() / 2 ) +
-                            Util.linear( PHScaleConstants.PH_RANGE.min, PHScaleConstants.PH_RANGE.max, SCALE_SIZE.height, 0, value || 7 );
-    } );
+    //TODO position probe
 
     expandedProperty.link( function( expanded ) {
-      verticalLineNode.visible = scaleNode.visible = pointerNode.visible = expanded;
+      //TODO probeNode.visible = expanded;
     } );
 
     thisNode.mutate( options );

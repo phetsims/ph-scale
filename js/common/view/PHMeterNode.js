4b3cc542f3bf122e73a52156f53f50234bc56fb9
pixelzoom <cmalley@pixelzoom.com>
2020-02-06 21:46:51 -0700

    use NumberSpinner in pH meter, #95

--- js/common/view/PHMeterNode.js
+++ js/common/view/PHMeterNode.js
@@ -14,37 +14,29 @@ define( require => {
 
   // modules
   const AccordionBox = require( 'SUN/AccordionBox' );
-  const ArrowButton = require( 'SUN/buttons/ArrowButton' );
-  const MathSymbols = require( 'SCENERY_PHET/MathSymbols' );
   const merge = require( 'PHET_CORE/merge' );
   const Node = require( 'SCENERY/nodes/Node' );
   const NumberDisplay = require( 'SCENERY_PHET/NumberDisplay' );
   const NumberProperty = require( 'AXON/NumberProperty' );
+  const NumberSpinner = require( 'SUN/NumberSpinner' );
   const Path = require( 'SCENERY/nodes/Path' );
   const PhetFont = require( 'SCENERY_PHET/PhetFont' );
   const phScale = require( 'PH_SCALE/phScale' );
   const PHScaleColors = require( 'PH_SCALE/common/PHScaleColors' );
   const PHScaleConstants = require( 'PH_SCALE/common/PHScaleConstants' );
+  const Property = require( 'AXON/Property' );
   const Rectangle = require( 'SCENERY/nodes/Rectangle' );
   const Shape = require( 'KITE/Shape' );
   const Solute = require( 'PH_SCALE/common/model/Solute' );
   const Tandem = require( 'TANDEM/Tandem' );
   const Text = require( 'SCENERY/nodes/Text' );
-  const Utils = require( 'DOT/Utils' );
 
   // strings
   const pHString = require( 'string!PH_SCALE/pH' );
-  const stringNoValue = MathSymbols.NO_VALUE;
 
   // constants
-  const X_MARGIN = 14;
   const Y_MARGIN = 10;
   const CORNER_RADIUS = 12;
-  const SPINNER_DELTA = 0.01;
-  const SPINNER_X_SPACING = 6;
-  const SPINNER_Y_SPACING = 4;
-  const SPINNER_TIMER_INTERVAL = 40; // ms
-  const SPINNER_ARROW_COLOR = 'rgb( 0, 200, 0 )';
 
   class PHMeterNode extends AccordionBox {
 
@@ -62,15 +54,16 @@ define( require => {
         fill: PHScaleColors.PANEL_FILL,
         lineWidth: 2,
         cornerRadius: CORNER_RADIUS,
-        titleAlignX: 'left',
         contentYSpacing: 10,
+        titleAlignX: 'left',
+        titleXMargin: 15,
         titleNode: new Text( pHString, {
           fill: 'black',
           font: new PhetFont( { size: 28, weight: 'bold' } ),
           maxWidth: 50
         } ),
         buttonAlign: 'right',
-        buttonXMargin: X_MARGIN,
+        buttonXMargin: 14,
         buttonYMargin: Y_MARGIN,
         expandCollapseButtonOptions: {
           sideLength: PHScaleConstants.EXPAND_COLLAPSE_BUTTON_LENGTH,
@@ -131,11 +124,10 @@ define( require => {
 
   phScale.register( 'PHMeterNode', PHMeterNode );
 
-  //TOD #95 use NumberSpinner here, and maybe DynamicProperty
   /**
    * Spinner for pH value.
    */
-  class PHSpinnerNode extends Node {
+  class PHSpinnerNode extends NumberSpinner {
 
     /**
      * @param {Solution} solution
@@ -145,104 +137,41 @@ define( require => {
 
       options = merge( {
 
+        // NumberSpinner options
+        valueAlign: 'right',
+        decimalPlaces: PHScaleConstants.PH_METER_DECIMAL_PLACES,
+        deltaValue: 0.01,
+        cornerRadius: CORNER_RADIUS,
+        arrowsScale: 1.5,
+        xMargin: 10,
+        yMargin: 4,
+        xSpacing: 6,
+        ySpacing: 4,
+        touchAreaXDilation: 15,
+        touchAreaYDilation: 2,
+        backgroundStroke: 'darkGray',
+
         // phet-io
         tandem: Tandem.REQUIRED
       }, options );
 
-      super( options );
-
-      // pH value
-      const pH = Utils.toFixed( PHScaleConstants.PH_RANGE.max, PHScaleConstants.PH_METER_DECIMAL_PLACES );
-      const pHText = new Text( pH, {
-        fill: 'black',
-        font: new PhetFont( 28 )
-      } );
-
-      // rectangle that the value is displayed in
-      const valueXMargin = 8;
-      const valueYMargin = 5;
-      const valueRectangle = new Rectangle( 0, 0, pHText.width + ( 2 * valueXMargin ), pHText.height + ( 2 * valueYMargin ), {
-        cornerRadius: CORNER_RADIUS,
-        fill: 'white',
-        stroke: 'darkGray'
-      } );
-
-      // layout
-      pHText.right = valueRectangle.right - valueXMargin;
-      pHText.centerY = valueRectangle.centerY;
-
-      // parent for all components related to the value
-      const valueNode = new Node( {
-        children: [ valueRectangle, pHText ]
-      } );
-
-      // sync with pH value
-      solution.pHProperty.link( pH => {
-        if ( pH === null ) {
-          pHText.text = stringNoValue;
-          pHText.centerX = valueRectangle.centerX; // center justified
-        }
-        else {
-          pHText.text = Utils.toFixed( pH, PHScaleConstants.PH_METER_DECIMAL_PLACES );
-          pHText.right = valueRectangle.right - valueXMargin; // right justified
-        }
-      } );
-
-      // options common to both arrow buttons
-      const arrowButtonOptions = { fireOnHoldInterval: SPINNER_TIMER_INTERVAL, enabledFill: SPINNER_ARROW_COLOR };
-
-      // button to increment value
-      const incrementButton = new ArrowButton( 'up',
-        () => {
-          pHValueProperty.set( Math.min( PHScaleConstants.PH_RANGE.max, solution.pHProperty.get() + SPINNER_DELTA ) );
-        },
-        merge( {
-          left: valueRectangle.right + SPINNER_X_SPACING,
-          bottom: valueRectangle.centerY - ( SPINNER_Y_SPACING / 2 ),
-          tandem: options.tandem.createTandem( 'incrementButton' )
-        }, arrowButtonOptions )
-      );
-      valueNode.addChild( incrementButton );
-
-      // button to decrement value
-      const decrementButton = new ArrowButton( 'down',
-        () => {
-          pHValueProperty.set( Math.max( PHScaleConstants.PH_RANGE.min, solution.pHProperty.get() - SPINNER_DELTA ) );
-        },
-        merge( {
-          left: incrementButton.left,
-          top: incrementButton.bottom + SPINNER_Y_SPACING,
-          tandem: options.tandem.createTandem( 'decrementButton' )
-        }, arrowButtonOptions )
-      );
-      valueNode.addChild( decrementButton );
-
-      // touch areas, expanded mostly to the right
-      const xDilation = incrementButton.width / 2;
-      const yDilation = 6;
-      incrementButton.touchArea = incrementButton.localBounds.dilatedXY( xDilation, yDilation ).shifted( xDilation, -yDilation );
-      decrementButton.touchArea = decrementButton.localBounds.dilatedXY( xDilation, yDilation ).shifted( xDilation, yDilation );
-
-      /*
+      /**
        * solution.pHProperty is derived, so we can't change it directly.
-       * So when pH changes, create a new custom solution with the desired pH.
+       * So when pH changes, create a new custom solute with the desired pH.
        */
       const pHValueProperty = new NumberProperty( solution.pHProperty.get(), {
-        reentrant: true
-      } );
-      solution.pHProperty.link( pH => {
-        pHValueProperty.set( pH );
+        reentrant: true //TODO see https://github.com/phetsims/ph-scale/issues/72
       } );
       pHValueProperty.link( pH => {
         if ( pH !== null && pH !== solution.pHProperty.get() ) {
           solution.soluteProperty.set( Solute.createCustom( pH ) );
         }
-        incrementButton.enabled = ( pH < PHScaleConstants.PH_RANGE.max );
-        decrementButton.enabled = ( pH > PHScaleConstants.PH_RANGE.min );
+      } );
+      solution.pHProperty.link( pH => {
+        pHValueProperty.set( pH );
       } );
 
-      // rendering order
-      this.addChild( valueNode );
+      super( pHValueProperty, new Property( PHScaleConstants.PH_RANGE ), options );
     }
   }
 

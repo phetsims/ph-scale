e7b9bf4c46997e7dadf2dec0ab7bd75318154686
pixelzoom <cmalley@pixelzoom.com>
2021-07-19 12:16:52 -0600

    round H3O+ and OH- values to 2 non-zero decimal places, https://github.com/phetsims/ph-scale/issues/225

--- js/common/view/graph/GraphIndicatorDragListener.js
+++ js/common/view/graph/GraphIndicatorDragListener.js
@@ -7,6 +7,7 @@
  */
 
 import Utils from '../../../../../dot/js/Utils.js';
+import ScientificNotationNode from '../../../../../scenery-phet/js/ScientificNotationNode.js';
 import DragListener from '../../../../../scenery/js/listeners/DragListener.js';
 import Tandem from '../../../../../tandem/js/Tandem.js';
 import phScale from '../../../phScale.js';
@@ -48,14 +49,29 @@ class GraphIndicatorDragListener extends DragListener {
           // Adjust the y-coordinate for the offset between the pointer and the indicator's origin
           const y = targetNode.globalToParentPoint( event.pointer.point ).y - clickYOffset;
 
-          // Convert the y-coordinate to a model value
+          // Convert the y-coordinate to a model value.
           const value = yToValue( y );
+          assert && assert( value > 0 );
+
+          // Round the model value to the first 2 non-zero decimal places. This prevents continuous dragging from
+          // creating values that have too much precision, which can result in pH = 7.00 with unequal amounts of 
+          // H3O+ and OH-. See https://github.com/phetsims/ph-scale/issues/225.
+          const scientificNotation = ScientificNotationNode.toScientificNotation( value, {
+            mantissaDecimalPlaces: PHScaleConstants.LOGARITHMIC_MANTISSA_DECIMAL_PLACES
+          } );
+          const exponent = scientificNotation.exponent - PHScaleConstants.LOGARITHMIC_MANTISSA_DECIMAL_PLACES;
+          const interval = Math.pow( 10, exponent );
+          const adjustedValue = Utils.roundToInterval( value, interval );
 
           // Map the model value to pH, depending on which units we're using.
-          let pH = ( graphUnitsProperty.get() === GraphUnits.MOLES_PER_LITER ) ? concentrationToPH( value ) : molesToPH( value, totalVolumeProperty.get() );
+          let pH = ( graphUnitsProperty.get() === GraphUnits.MOLES_PER_LITER ) ?
+                   concentrationToPH( adjustedValue ) :
+                   molesToPH( adjustedValue, totalVolumeProperty.get() );
 
           // Constrain the pH to the valid range
           pH = Utils.clamp( pH, PHScaleConstants.PH_RANGE.min, PHScaleConstants.PH_RANGE.max );
+
+          phet.log && phet.log( `value=${value} adjustedValue=${adjustedValue} pH=${pH}` );
 
           // Set the solution's pH
           pHProperty.set( pH );

b791f8e74270f7f607267a26ff72dd1b7ad780a3
phet-dev <phetadmin@gmail.com>
2022-03-05 03:26:59 -0700

    update copyright dates from daily grunt work

--- js/common/view/graph/GraphIndicatorNode.js
+++ js/common/view/graph/GraphIndicatorNode.js
@@ -1,4 +1,4 @@
-// Copyright 2013-2021, University of Colorado Boulder
+// Copyright 2013-2022, University of Colorado Boulder
 
 /**
  * GraphIndicatorNode points to a value on a graph's vertical scale.

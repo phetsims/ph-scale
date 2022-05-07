ff1e944d9f0ab2601d4b94ddac43119b171419b7
Sam Reid <reids@colorado.edu>
2022-05-07 09:36:57 -0600

    Use semicolons to terminate fields in type StringType, see https://github.com/phetsims/chipper/issues/1208

--- js/phScaleStrings.ts
+++ js/phScaleStrings.ts
@@ -9,48 +9,48 @@ import phScale from './phScale.js';
 
 type StringsType = {
   'ph-scale': {
-    'title': string
-  },
+    'title': string;
+  };
   'screen': {
-    'macro': string,
-    'micro': string,
-    'mySolution': string
-  },
+    'macro': string;
+    'micro': string;
+    'mySolution': string;
+  };
   'choice': {
-    'milk': string,
-    'chickenSoup': string,
-    'batteryAcid': string,
-    'vomit': string,
-    'soda': string,
-    'orangeJuice': string,
-    'coffee': string,
-    'spit': string,
-    'blood': string,
-    'handSoap': string,
-    'water': string,
-    'drainCleaner': string
-  },
+    'milk': string;
+    'chickenSoup': string;
+    'batteryAcid': string;
+    'vomit': string;
+    'soda': string;
+    'orangeJuice': string;
+    'coffee': string;
+    'spit': string;
+    'blood': string;
+    'handSoap': string;
+    'water': string;
+    'drainCleaner': string;
+  };
   'units': {
-    'liters': string,
-    'moles': string,
-    'molesPerLiter': string
-  },
+    'liters': string;
+    'moles': string;
+    'molesPerLiter': string;
+  };
   'pattern': {
     '0value': {
-      '1units': string
+      '1units': string;
     }
-  },
-  'pH': string,
-  'acidic': string,
-  'basic': string,
-  'neutral': string,
-  'concentration': string,
-  'quantity': string,
-  'moleculeCount': string,
-  'ratio': string,
-  'linear': string,
-  'logarithmic': string,
-  'offScale': string
+  };
+  'pH': string;
+  'acidic': string;
+  'basic': string;
+  'neutral': string;
+  'concentration': string;
+  'quantity': string;
+  'moleculeCount': string;
+  'ratio': string;
+  'linear': string;
+  'logarithmic': string;
+  'offScale': string;
 };
 
 const phScaleStrings = getStringModule( 'PH_SCALE' ) as StringsType;

7dbe20adcf4c4461e072e62b3f6e9b6104fca82b
Jonathan Olson <jonathan.olson@colorado.edu>
2022-08-15 20:36:42 -0600

    Grunt update, and changes for string Properties, see https://github.com/phetsims/chipper/issues/1302

--- js/phScaleStrings.ts
+++ js/phScaleStrings.ts
@@ -5,52 +5,84 @@
  */
 /* eslint-disable */
 import getStringModule from '../../chipper/js/getStringModule.js';
+import TReadOnlyProperty from '../../axon/js/TReadOnlyProperty.js';
 import phScale from './phScale.js';
 
 type StringsType = {
   'ph-scale': {
     'title': string;
+    'titleProperty': TReadOnlyProperty<string>;
   };
   'screen': {
     'macro': string;
+    'macroProperty': TReadOnlyProperty<string>;
     'micro': string;
+    'microProperty': TReadOnlyProperty<string>;
     'mySolution': string;
+    'mySolutionProperty': TReadOnlyProperty<string>;
   };
   'choice': {
     'milk': string;
+    'milkProperty': TReadOnlyProperty<string>;
     'chickenSoup': string;
+    'chickenSoupProperty': TReadOnlyProperty<string>;
     'batteryAcid': string;
+    'batteryAcidProperty': TReadOnlyProperty<string>;
     'vomit': string;
+    'vomitProperty': TReadOnlyProperty<string>;
     'soda': string;
+    'sodaProperty': TReadOnlyProperty<string>;
     'orangeJuice': string;
+    'orangeJuiceProperty': TReadOnlyProperty<string>;
     'coffee': string;
+    'coffeeProperty': TReadOnlyProperty<string>;
     'spit': string;
+    'spitProperty': TReadOnlyProperty<string>;
     'blood': string;
+    'bloodProperty': TReadOnlyProperty<string>;
     'handSoap': string;
+    'handSoapProperty': TReadOnlyProperty<string>;
     'water': string;
+    'waterProperty': TReadOnlyProperty<string>;
     'drainCleaner': string;
+    'drainCleanerProperty': TReadOnlyProperty<string>;
   };
   'units': {
     'liters': string;
+    'litersProperty': TReadOnlyProperty<string>;
     'moles': string;
+    'molesProperty': TReadOnlyProperty<string>;
     'molesPerLiter': string;
+    'molesPerLiterProperty': TReadOnlyProperty<string>;
   };
   'pattern': {
     '0value': {
       '1units': string;
+      '1unitsProperty': TReadOnlyProperty<string>;
     }
   };
   'pH': string;
+  'pHProperty': TReadOnlyProperty<string>;
   'acidic': string;
+  'acidicProperty': TReadOnlyProperty<string>;
   'basic': string;
+  'basicProperty': TReadOnlyProperty<string>;
   'neutral': string;
+  'neutralProperty': TReadOnlyProperty<string>;
   'concentration': string;
+  'concentrationProperty': TReadOnlyProperty<string>;
   'quantity': string;
+  'quantityProperty': TReadOnlyProperty<string>;
   'moleculeCount': string;
+  'moleculeCountProperty': TReadOnlyProperty<string>;
   'ratio': string;
+  'ratioProperty': TReadOnlyProperty<string>;
   'linear': string;
+  'linearProperty': TReadOnlyProperty<string>;
   'logarithmic': string;
+  'logarithmicProperty': TReadOnlyProperty<string>;
   'offScale': string;
+  'offScaleProperty': TReadOnlyProperty<string>;
 };
 
 const phScaleStrings = getStringModule( 'PH_SCALE' ) as StringsType;

31ce783f011070fbfc0f42e17f68f70a7e554237
pixelzoom <cmalley@pixelzoom.com>
2022-09-04 13:03:20 -0600

    convert to TS, https://github.com/phetsims/ph-scale/issues/242

--- js/common/view/graph/GraphUnitsSwitch.ts
+++ js/common/view/graph/GraphUnitsSwitch.ts
@@ -1,38 +1,39 @@
 // Copyright 2020-2022, University of Colorado Boulder
 
-// @ts-nocheck
 /**
  * GraphScaleSwitch is the control for switching between Concentration and Quantity units for the graphs.
  *
  * @author Chris Malley (PixelZoom, Inc.)
  */
 
+import EnumerationProperty from '../../../../../axon/js/EnumerationProperty.js';
 import Dimension2 from '../../../../../dot/js/Dimension2.js';
-import merge from '../../../../../phet-core/js/merge.js';
+import optionize, { EmptySelfOptions } from '../../../../../phet-core/js/optionize.js';
+import PickRequired from '../../../../../phet-core/js/types/PickRequired.js';
 import { RichText } from '../../../../../scenery/js/imports.js';
-import ABSwitch from '../../../../../sun/js/ABSwitch.js';
+import ABSwitch, { ABSwitchOptions } from '../../../../../sun/js/ABSwitch.js';
 import phScale from '../../../phScale.js';
 import phScaleStrings from '../../../phScaleStrings.js';
 import PHScaleConstants from '../../PHScaleConstants.js';
 import GraphUnits from './GraphUnits.js';
 
-class GraphUnitsSwitch extends ABSwitch {
+type SelfOptions = EmptySelfOptions;
 
-  /**
-   * @param {EnumerationProperty.<GraphUnits>} graphUnitsProperty
-   * @param {Object} [options]
-   */
-  constructor( graphUnitsProperty, options ) {
+export type GraphUnitsSwitchOptions = SelfOptions & PickRequired<ABSwitchOptions, 'tandem'>;
 
-    options = merge( {
+export default class GraphUnitsSwitch extends ABSwitch<GraphUnits> {
+
+  public constructor( graphUnitsProperty: EnumerationProperty<GraphUnits>, provideOptions: GraphUnitsSwitchOptions ) {
+
+    const options = optionize<GraphUnitsSwitchOptions, SelfOptions, ABSwitchOptions>()( {
+
+      // ABSwitchOptions
       toggleSwitchOptions: { size: new Dimension2( 50, 25 ) },
       centerOnSwitch: true,
-
-      // phet-io
-      tandem: options.tandem.createTandem( 'graphUnitsSwitch' ),
       phetioDocumentation: 'A/B switch for switching units'
-    }, options );
+    }, provideOptions );
 
+    // Concentration (mol/L)
     const concentrationText = new RichText( `${phScaleStrings.concentration}<br>(${phScaleStrings.units.molesPerLiter})`, {
       align: 'center',
       font: PHScaleConstants.AB_SWITCH_FONT,
@@ -40,6 +41,7 @@ class GraphUnitsSwitch extends ABSwitch {
       tandem: options.tandem.createTandem( 'concentrationText' )
     } );
 
+    // Quantity (mol)
     const quantityText = new RichText( `${phScaleStrings.quantity}<br>(${phScaleStrings.units.moles})`, {
       align: 'center',
       font: PHScaleConstants.AB_SWITCH_FONT,
@@ -52,4 +54,3 @@ class GraphUnitsSwitch extends ABSwitch {
 }
 
 phScale.register( 'GraphUnitsSwitch', GraphUnitsSwitch );
-export default GraphUnitsSwitch;

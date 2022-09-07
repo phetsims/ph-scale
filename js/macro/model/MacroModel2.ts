0e0af3450555081441a92668c5dca0212fac8b7d
pixelzoom <cmalley@pixelzoom.com>
2022-09-07 15:03:14 -0600

    rename PHModelOptions, https://github.com/phetsims/ph-scale/issues/242

--- js/macro/model/MacroModel2.ts
+++ js/macro/model/MacroModel2.ts
@@ -9,19 +9,19 @@
 
 import optionize, { EmptySelfOptions } from '../../../../phet-core/js/optionize.js';
 import PickRequired from '../../../../phet-core/js/types/PickRequired.js';
-import PHModel, { MacroModelOptions } from '../../common/model/PHModel.js';
+import PHModel, { PHModelOptions } from '../../common/model/PHModel.js';
 import phScale from '../../phScale.js';
 import MacroSolution from './MacroSolution.js';
 
 type SelfOptions = EmptySelfOptions;
 
-export type MacroModel2Options = SelfOptions & PickRequired<MacroModelOptions<MacroSolution>, 'tandem'>;
+export type MacroModel2Options = SelfOptions & PickRequired<PHModelOptions<MacroSolution>, 'tandem'>;
 
 export default class MacroModel2 extends PHModel<MacroSolution> {
 
   public constructor( providedOptions: MacroModel2Options ) {
 
-    const options = optionize<MacroModel2Options, SelfOptions, MacroModelOptions<MacroSolution>>()( {
+    const options = optionize<MacroModel2Options, SelfOptions, PHModelOptions<MacroSolution>>()( {
 
       // Creates the solution needed by the Macro screen
       createSolution: ( solutionProperty, maxVolume, tandem ) => new MacroSolution( solutionProperty, {

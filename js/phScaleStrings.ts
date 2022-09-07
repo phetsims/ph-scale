7c57a1d6ed6b57af43661fa4d92199f876a1a197
pixelzoom <cmalley@pixelzoom.com>
2022-09-07 11:29:30 -0600

    support for dynamic locale, https://github.com/phetsims/ph-scale/issues/239

--- js/phScaleStrings.ts
+++ js/phScaleStrings.ts
@@ -10,78 +10,47 @@ import phScale from './phScale.js';
 
 type StringsType = {
   'ph-scale': {
-    'title': string;
     'titleStringProperty': TReadOnlyProperty<string>;
   };
   'screen': {
-    'macro': string;
     'macroStringProperty': TReadOnlyProperty<string>;
-    'micro': string;
     'microStringProperty': TReadOnlyProperty<string>;
-    'mySolution': string;
     'mySolutionStringProperty': TReadOnlyProperty<string>;
   };
   'choice': {
-    'milk': string;
     'milkStringProperty': TReadOnlyProperty<string>;
-    'chickenSoup': string;
     'chickenSoupStringProperty': TReadOnlyProperty<string>;
-    'batteryAcid': string;
     'batteryAcidStringProperty': TReadOnlyProperty<string>;
-    'vomit': string;
     'vomitStringProperty': TReadOnlyProperty<string>;
-    'soda': string;
     'sodaStringProperty': TReadOnlyProperty<string>;
-    'orangeJuice': string;
     'orangeJuiceStringProperty': TReadOnlyProperty<string>;
-    'coffee': string;
     'coffeeStringProperty': TReadOnlyProperty<string>;
-    'spit': string;
     'spitStringProperty': TReadOnlyProperty<string>;
-    'blood': string;
     'bloodStringProperty': TReadOnlyProperty<string>;
-    'handSoap': string;
     'handSoapStringProperty': TReadOnlyProperty<string>;
-    'water': string;
     'waterStringProperty': TReadOnlyProperty<string>;
-    'drainCleaner': string;
     'drainCleanerStringProperty': TReadOnlyProperty<string>;
   };
   'units': {
-    'liters': string;
     'litersStringProperty': TReadOnlyProperty<string>;
-    'moles': string;
     'molesStringProperty': TReadOnlyProperty<string>;
-    'molesPerLiter': string;
     'molesPerLiterStringProperty': TReadOnlyProperty<string>;
   };
   'pattern': {
     '0value': {
-      '1units': string;
       '1unitsStringProperty': TReadOnlyProperty<string>;
     }
   };
-  'pH': string;
   'pHStringProperty': TReadOnlyProperty<string>;
-  'acidic': string;
   'acidicStringProperty': TReadOnlyProperty<string>;
-  'basic': string;
   'basicStringProperty': TReadOnlyProperty<string>;
-  'neutral': string;
   'neutralStringProperty': TReadOnlyProperty<string>;
-  'concentration': string;
   'concentrationStringProperty': TReadOnlyProperty<string>;
-  'quantity': string;
   'quantityStringProperty': TReadOnlyProperty<string>;
-  'moleculeCount': string;
   'moleculeCountStringProperty': TReadOnlyProperty<string>;
-  'ratio': string;
   'ratioStringProperty': TReadOnlyProperty<string>;
-  'linear': string;
   'linearStringProperty': TReadOnlyProperty<string>;
-  'logarithmic': string;
   'logarithmicStringProperty': TReadOnlyProperty<string>;
-  'offScale': string;
   'offScaleStringProperty': TReadOnlyProperty<string>;
 };
 

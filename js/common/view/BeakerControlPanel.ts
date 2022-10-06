7de32b8cc9031d90db3655ebd98b7b19d68e5036
pixelzoom <cmalley@pixelzoom.com>
2022-10-06 12:48:57 -0600

    instrument additional Text and Properties, https://github.com/phetsims/ph-scale/issues/251

--- js/common/view/BeakerControlPanel.ts
+++ js/common/view/BeakerControlPanel.ts
@@ -13,9 +13,11 @@ import optionize, { EmptySelfOptions } from '../../../../phet-core/js/optionize.
 import PickRequired from '../../../../phet-core/js/types/PickRequired.js';
 import StringUtils from '../../../../phetcommon/js/util/StringUtils.js';
 import PhetFont from '../../../../scenery-phet/js/PhetFont.js';
-import { RichText, Text, VBox, HSeparator } from '../../../../scenery/js/imports.js';
+import { HSeparator, RichText, Text, VBox } from '../../../../scenery/js/imports.js';
 import Checkbox from '../../../../sun/js/Checkbox.js';
 import Panel, { PanelOptions } from '../../../../sun/js/Panel.js';
+import Tandem from '../../../../tandem/js/Tandem.js';
+import StringIO from '../../../../tandem/js/types/StringIO.js';
 import phScale from '../../phScale.js';
 import PhScaleStrings from '../../PhScaleStrings.js';
 import PHScaleColors from '../PHScaleColors.js';
@@ -43,26 +45,12 @@ export default class BeakerControlPanel extends Panel {
       phetioDocumentation: 'control panel that appears below the beaker'
     }, providedOptions );
 
-    // 'H3O+ / OH- Ratio' checkbox, with color-coded symbols
-    const ratioStringProperty = new DerivedProperty(
-      [ PhScaleStrings.ratioStringProperty ],
-      ( ratioString: string ) => StringUtils.fillIn( `{{h3o}} / {{oh}} ${ratioString} `, {
-        h3o: `<span style="color:${PHScaleColors.H3O_MOLECULES.toCSS()}">${PHScaleConstants.H3O_FORMULA}</span>`,
-        oh: `<span style="color:${PHScaleColors.OH_MOLECULES.toCSS()}">${PHScaleConstants.OH_FORMULA}</span>`
-      } ) );
-    const ratioText = new RichText( ratioStringProperty, {
-      font: FONT
-    } );
-    const ratioCheckbox = new Checkbox( ratioVisibleProperty, ratioText, {
-      tandem: options.tandem.createTandem( 'ratioCheckbox' )
-    } );
+    // 'H3O+ / OH- Ratio' checkbox
+    const ratioCheckbox = new RatioCheckbox( ratioVisibleProperty, options.tandem.createTandem( 'ratioCheckbox' ) );
     ratioCheckbox.touchArea = ratioCheckbox.localBounds.dilatedXY( 10, 6 );
 
-    // 'Molecule count' checkbox
-    const moleculeCountLabelText = new Text( PhScaleStrings.moleculeCountStringProperty, { font: FONT } );
-    const moleculeCountCheckbox = new Checkbox( moleculeCountVisibleProperty, moleculeCountLabelText, {
-      tandem: options.tandem.createTandem( 'moleculeCountCheckbox' )
-    } );
+    // 'Molecule Count' checkbox
+    const moleculeCountCheckbox = new MoleculeCountCheckbox( moleculeCountVisibleProperty, options.tandem.createTandem( 'moleculeCountCheckbox' ) );
     moleculeCountCheckbox.touchArea = ratioCheckbox.localBounds.dilatedXY( 10, 6 );
 
     const separator = new HSeparator( {
@@ -76,6 +64,51 @@ export default class BeakerControlPanel extends Panel {
     } );
 
     super( content, options );
+  }
+}
+
+/**
+ * 'H3O+ / OH- Ratio' checkbox, with color-coded symbols
+ */
+class RatioCheckbox extends Checkbox {
+
+  public constructor( ratioVisibleProperty: Property<boolean>, tandem: Tandem ) {
+
+    const stringProperty = new DerivedProperty(
+      [ PhScaleStrings.ratioStringProperty ],
+      ( ratioString: string ) => StringUtils.fillIn( `{{h3o}} / {{oh}} ${ratioString} `, {
+        h3o: `<span style="color:${PHScaleColors.H3O_MOLECULES.toCSS()}">${PHScaleConstants.H3O_FORMULA}</span>`,
+        oh: `<span style="color:${PHScaleColors.OH_MOLECULES.toCSS()}">${PHScaleConstants.OH_FORMULA}</span>`
+      } ), {
+        tandem: tandem.createTandem( 'stringProperty' ),
+        phetioValueType: StringIO
+      } );
+
+    const ratioText = new RichText( stringProperty, {
+      font: FONT
+    } );
+
+    super( ratioVisibleProperty, ratioText, {
+      tandem: tandem
+    } );
+  }
+}
+
+/**
+ * 'Molecule Count' checkbox
+ */
+class MoleculeCountCheckbox extends Checkbox {
+
+  public constructor( moleculeCountVisibleProperty: Property<boolean>, tandem: Tandem ) {
+
+    const text = new Text( PhScaleStrings.moleculeCountStringProperty, {
+      font: FONT,
+      tandem: tandem.createTandem( 'text' )
+    } );
+
+    super( moleculeCountVisibleProperty, text, {
+      tandem: tandem
+    } );
   }
 }
 

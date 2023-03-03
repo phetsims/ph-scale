6d1cb6f54a800261aaf0086d59adab25ebe9d314
phet-dev <phetadmin@gmail.com>
2023-03-03 05:13:14 -0700

    update copyright dates from daily grunt work

--- js/mysolution/view/MySolutionPHAccordionBox.ts
+++ js/mysolution/view/MySolutionPHAccordionBox.ts
@@ -1,4 +1,4 @@
-// Copyright 2022, University of Colorado Boulder
+// Copyright 2022-2023, University of Colorado Boulder
 
 /**
  * MySolutionPHAccordionBox is the pH accordion box (aka meter) for the 'My Solution' screen.

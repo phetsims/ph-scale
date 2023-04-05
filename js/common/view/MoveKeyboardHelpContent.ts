5f8363908d78827d2c992f61853b5b680a0609c1
phet-dev <phetadmin@gmail.com>
2023-04-05 05:06:46 -0600

    update copyright dates from daily grunt work

--- js/common/view/MoveKeyboardHelpContent.ts
+++ js/common/view/MoveKeyboardHelpContent.ts
@@ -1,4 +1,4 @@
-// Copyright 2022, University of Colorado Boulder
+// Copyright 2022-2023, University of Colorado Boulder
 
 /**
  * MoveKeyboardHelpContent is the keyboard-help section that describes how to move things that are draggable.

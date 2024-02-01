29ad3a04fc4e5dfba6c4a6e010ba4126ba565fae
phet-dev <phetadmin@gmail.com>
2024-02-01 04:13:46 -0700

    update copyright dates from daily grunt work

--- js/common/view/FaucetControlsKeyboardHelpContent.ts
+++ js/common/view/FaucetControlsKeyboardHelpContent.ts
@@ -1,4 +1,4 @@
-// Copyright 2022, University of Colorado Boulder
+// Copyright 2022-2024, University of Colorado Boulder
 
 //TODO https://github.com/phetsims/scenery-phet/issues/839 move to scenery-phet
 /**

82edeadcf948037d5e6845c39b299b514c535b2f
pixelzoom <cmalley@pixelzoom.com>
2025-04-15 10:49:45 -0600

    File documentation should precede import statements, https://github.com/phetsims/perennial/issues/447

--- js/common/view/graph/GraphUnits.ts
+++ js/common/view/graph/GraphUnits.ts
@@ -1,9 +1,5 @@
 // Copyright 2013-2022, University of Colorado Boulder
 
-import Enumeration from '../../../../../phet-core/js/Enumeration.js';
-import EnumerationValue from '../../../../../phet-core/js/EnumerationValue.js';
-import phScale from '../../../phScale.js';
-
 /**
  * Units used on the graph.
  * NOTE: When converting to TypeScript, this was not converted to a string union because we do not want to change
@@ -11,6 +7,10 @@ import phScale from '../../../phScale.js';
  *
  * @author Chris Malley (PixelZoom, Inc.)
  */
+
+import Enumeration from '../../../../../phet-core/js/Enumeration.js';
+import EnumerationValue from '../../../../../phet-core/js/EnumerationValue.js';
+import phScale from '../../../phScale.js';
 
 export default class GraphUnits extends EnumerationValue {
   public static readonly MOLES_PER_LITER = new GraphUnits();

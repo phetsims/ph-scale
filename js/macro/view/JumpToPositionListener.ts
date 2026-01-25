7804e832da5a736dc460d1d3d2bfe5bef4f30de6
phet-dev <phetadmin@gmail.com>
2026-01-25 04:16:30 -0700

    update copyright dates from daily grunt work

--- js/macro/view/JumpToPositionListener.ts
+++ js/macro/view/JumpToPositionListener.ts
@@ -1,4 +1,4 @@
-// Copyright 2025, University of Colorado Boulder
+// Copyright 2025-2026, University of Colorado Boulder
 
 /**
  * JumpToPositionListener is a keyboard listener specialized for the 'jump to useful position' shortcut.
